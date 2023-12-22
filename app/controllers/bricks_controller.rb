af1c090f43f90a5e25008be7703e694a06f7d67b
Constantin Lerner <colerner@me.com>
2023-12-22 23:08:59 +0100

    most of the controller actions done and tested, still finishing the simple form for adding new brick

---
+++ app/controllers/bricks_controller.rb
@@ -0,0 +1,54 @@
+class BricksController < ApplicationController
+  before_action :set_brick, only: %i[show edit update destroy]
+  skip_before_action :authenticate_user!, only: %i[index show]
+
+  def index
+    @bricks = Brick.all
+  end
+
+  def show
+  end
+
+  def new
+    @brick = Brick.new
+  end
+
+  def create
+    @brick = Brick.new(brick_params)
+    @brick.user = current_user
+    if @brick.save
+      redirect_to @brick, notice: "Brick was succesfully added to the market"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @brick.user = current_user
+    if @brick.update(brick_params)
+      redirect_to @brick, notice: "Brick was succesfully updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @brick.user = current_user
+    @brick.destroy!
+    redirect_to bricks_path, notice: "Brick was sucessfully deleted", status: :see_other
+  end
+
+  private
+
+  def set_brick
+    @brick = Brick.find(params[:id])
+  end
+
+  def brick_params
+    params.require(:brick).permit(:name, :description, :city, :address, :price, :brick_condition, :colour)
+  end
+end
