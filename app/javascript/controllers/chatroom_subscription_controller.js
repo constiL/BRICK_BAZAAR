24fe80092857b6e06bb985179b212be6077ffe04
Constantin Lerner <colerner@me.com>
2023-12-30 03:38:41 +0100

    added action cable and created stimulus controller and chatroom channel

---
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -0,0 +1,9 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="chatroom-subscription"
+export default class extends Controller {
+  static targets = ["messages"]
+  connect() {
+    console.log(this.messagesTarget)
+  }
+}
