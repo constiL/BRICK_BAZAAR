90e321e3c283ae3f8d8c6e8ee6bc6cdda3ab5656
Constantin Lerner <colerner@me.com>
2023-12-30 16:18:22 +0100

    added geocoder gem and updated the model

--- app/models/brick.rb
+++ app/models/brick.rb
@@ -8,4 +8,7 @@ class Brick < ApplicationRecord
     presence: true
   validates :name, length: { minimum: 5 }
   validates :price, numericality: { greater_than_or_equal_to: 1, only_integer: true }
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
